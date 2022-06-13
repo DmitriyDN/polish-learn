368be625b6ffb197570366b7f1f756de0b22c8a9
Dmitriy Beseda <dmitriy.beseda@fluxon.com>
2022-06-13 09:37:35 +0200

    scripts

---
+++ deploy-scripts/stop.backend.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+SRC_PATH=/home/dima/polish-learn
+SERVICE_NAME=lang-backend
+
+pm2 delete $SERVICE_NAME
---
+++ deploy-scripts/stop.frontend.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+SRC_PATH=/home/dima/polish-learn
+SERVICE_NAME=lang-frontend
+
+pm2 delete $SERVICE_NAME
